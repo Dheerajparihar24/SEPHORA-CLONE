8c8803a8ffa7aad668f9a32060c9b385cbd2a3ed
Dheeraj Parihar <dheerajparihar02001@gmail.com>
2024-10-09 10:11:18 +0530

    Hero Section Img

--- Sephora-Clone/src/data/heroSectionData.js
+++ Sephora-Clone/src/data/heroSectionData.js
@@ -1,38 +1,51 @@
 export const data = [
-    {
-      srcImg: "src/assets/images/HeroSection-1.webp",
-      label: "Decadent Vanilla Scents",
-      subLabel: "Spritz on what's trending this season.",
-      label2: " Shop Know",
-    },
-    {
-      srcImg: "src/assets/images/HeroSection-2.webp",
-      label: "Decadent Vanilla Scents",
-      subLabel: "Spritz on what's trending this season.",
-      label2: " Shop Know",
-    },
-    {
-      srcImg: "src/assets/images/HeroSection-3.avif",
-      label: "Decadent Vanilla Scents",
-      subLabel: "Spritz on what's trending this season.",
-      label2: " Shop Know",
-    },
-    {
-      srcImg: "src/assets/images/HeroSection-4.avif",
-      label: "Decadent Vanilla Scents",
-      subLabel: "Spritz on what's trending this season.",
-      label2: " Shop Know",
-    },
-    {
-      srcImg: "src/assets/images/HeroSection-5.avif",
-      label: "Decadent Vanilla Scents",
-      subLabel: "Spritz on what's trending this season.",
-      label2: " Shop Know",
-    },
-    {
-      srcImg: "src/assets/images/HeroSection-6.avif",
-      label: "Decadent Vanilla Scents",
-      subLabel: "Spritz on what's trending this season.",
-      label2: " Shop Know",
-    },
-  ];
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-09-dry-shampoo-mbc-site-home-page-RWD-hero-banner-2833-US-CAN-image.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-oct-oh-hair-yeah-site-desktop-home-page-rwd-hero-banner-generic-1200x800-us-can-2833.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-10-07-slotting-october-minis-v2-site-rwd-home-page-hero-banner-US-handoff-slice-01.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-10-07-slotting-october-minis-v2-site-rwd-home-page-hero-banner-US-handoff-slice-01.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-q3-fragrance-site-home-page-RWD-hero-banner-newness-us-can-2957-image-only.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-aug-acne-mbc-site-desktop-mobile-app-home-page-rwd-hero-banner-post-acne-1200x800-can-3018.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+  {
+    srcImg:
+      "https://www.sephora.com/contentimages/2024-holiday-preview-site-home-page-RWD-hero-banner-general-preview-new-exclusive-v2-us-can-2995-artwork-only.jpg?imwidth=545",
+    label: "Decadent Vanilla Scents",
+    subLabel: "Spritz on what's trending this season.",
+    label2: " Shop Know",
+  },
+];
